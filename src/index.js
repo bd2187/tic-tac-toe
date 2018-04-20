12ce9df214493418c48850d7f925367aed0d4d05
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-04-19 19:46:52 -0700

    Implement constructor method in Square class and change value property of state object whenever button is clicked

--- src/index.js
+++ src/index.js
@@ -3,15 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: null
+        }
+    }
+
     render() {
-      return (
-        <button className="square" onClick={() => console.log(this.props.value)}>
-          {this.props.value}
-        </button>
-      );
+        return (
+            <button className="square" onClick={()=>{this.setState({value: 'X'})}}>
+                {this.state.value}
+            </button>
+        )
     }
 }
   
+
 class Board extends React.Component {
     renderSquare(i) {
         return <Square value={i}/>;
